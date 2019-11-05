cde52b8b42bd2cad696134140953a4ec57c8ea0f
Gustavo Rosa <gth.rosa@uol.com.br>
2019-11-05 10:55:23 -0300

    I have lied. Adding possibility to use the script n_runs.

--- experiments/ensemble_learner.sh
+++ experiments/ensemble_learner.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # Initial variables
-DATASET=cifar10
+DATASET=mnist
 N_MODELS=15
+RUNS=15
 
 # Looping for every possible model
 for (( i=0; i<$N_MODELS; i++ ))
@@ -11,5 +12,10 @@ do
     VAL_PREDS=""$VAL_PREDS"predictions/"$DATASET"_random_"$i".txt "
 done
 
-# Running the actual ensemble learner
-python ensemble_learner.py predictions/"$DATASET"_random_labels.txt predictions/"$DATASET"_random_labels_tst.txt -val_preds $VAL_PREDS --show_test > "$DATASET"_random_ensemble.txt 2>&1 &
+# Looping for every possible running
+for (( j=0; j<$RUNS; j++ ))
+do
+    # Running the actual ensemble learner
+    python ensemble_learner.py predictions/"$DATASET"_random_labels.txt predictions/"$DATASET"_random_labels_tst.txt -val_preds $VAL_PREDS --show_test > "$DATASET"_random_ensemble_"$j".txt 2>&1 &
+done
+
