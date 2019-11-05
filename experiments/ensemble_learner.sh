33c5a09150f52ca62bbb2ae40c543cb0df2f7a5e
Gustavo Rosa <gth.rosa@uol.com.br>
2019-11-05 10:47:29 -0300

    Adding shell script to ease the running of ensemble_learner.py.

---
+++ experiments/ensemble_learner.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Initial variables
+DATASET=mnist
+N_MODELS=10
+
+# Looping for every possible model
+for (( i=0; i<$N_MODELS; i++ ))
+do
+    # Concatenating the string
+    VAL_PREDS=""$VAL_PREDS"predictions/"$DATASET"_random_"$i".txt "
+done
+
+# Running the actual ensemble learner
+python ensemble_learner.py predictions/"$DATASET"_random_labels.txt predictions/"$DATASET"_random_labels_tst.txt -val_preds $VAL_PREDS
